bcabaee4b7942fe5049fe44d29d9d34b6d1b5e24
Satoshi Konno <skonno@cybergarage.org>
2020-11-14 23:45:38 +0900

    Add FdtD88RawHeader and FdtD88RawTrack

--- include/fdtools/img/d88.h
+++ include/fdtools/img/d88.h
@@ -15,11 +15,56 @@
 #ifndef _FDTOOLS_IMG_D88_H_
 #define _FDTOOLS_IMG_D88_H_
 
-#include <stdlib.h>
+#include <fdtools/img/header.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+const byte D88_WRITE_PROTECT_NONE = 0x00;
+const byte D88_WRITE_PROTECT_ENABLED = 0x10;
+
+const byte D88_DISK_TYPE_2D = 0x00;
+const byte D88_DISK_TYPE_2DD = 0x10;
+const byte D88_DISK_TYPE_2HD = 0x20;
+const byte D88_DISK_TYPE_1D = 0x30;
+const byte D88_DISK_TYPE_1DD = 0x40;
+
+typedef struct FDT_ATTR_PACKED {
+  char name[17];
+  byte reserve[9];
+  byte write_protect;
+  byte disk_type;
+  long disk_size;
+  long track_offset[164];
+} FdtD88RawHeader;
+
+const byte D88_TRACK_DELETED_MARK_NONE = 0x00;
+const byte D88_TRACK_DELETED_MARK_ENABLED = 0x10;
+
+const byte D88_TRACK_DENSITY_SINGLE = 0x40;
+const byte D88_TRACK_DENSITY_DOUBLE = 0x00;
+const byte D88_TRACK_DENSITY_HIGH = 0x01;
+
+const byte D88_TRACK_STATUS_NORMAL = 0x00;
+const byte D88_TRACK_STATUS_NORMA_DELETED = 0x10;
+const byte D88_TRACK_STATUS_CRC_ERROR = 0xA0;
+const byte D88_TRACK_STATUS_DATS_CRC_ERROR = 0xB0;
+const byte D88_TRACK_STATUS_NON_ADDRESS_MARK = 0xE0;
+const byte D88_TRACK_STATUS_NON_DATA_MARK = 0xF0;
+
+typedef struct FDT_ATTR_PACKED {
+  byte c;
+  byte h;
+  byte r;
+  byte n;
+  uint8_t number_of_sector;
+  byte deleted_mark;
+  byte status;
+  byte reserve[5];
+  uint8_t size_of_data;
+  byte* data;
+} FdtD88RawTrack;
 
 #ifdef __cplusplus
 } /* extern C */
