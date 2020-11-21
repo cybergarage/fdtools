cb10b99415b51a97e920a6fad6ca1d574cfc4e4b
Satoshi Konno <skonno@cybergarage.org>
2020-11-21 16:41:17 +0900

    Update FdtD88RawHeader and FdtD88RawTrack

--- include/fdtools/img/d88.h
+++ include/fdtools/img/d88.h
@@ -21,49 +21,48 @@
 extern "C" {
 #endif
 
-const byte D88_WRITE_PROTECT_NONE = 0x00;
-const byte D88_WRITE_PROTECT_ENABLED = 0x10;
+const uint8_t D88_WRITE_PROTECT_NONE = 0x00;
+const uint8_t D88_WRITE_PROTECT_ENABLED = 0x10;
 
-const byte D88_DISK_TYPE_2D = 0x00;
-const byte D88_DISK_TYPE_2DD = 0x10;
-const byte D88_DISK_TYPE_2HD = 0x20;
-const byte D88_DISK_TYPE_1D = 0x30;
-const byte D88_DISK_TYPE_1DD = 0x40;
+const uint8_t D88_DISK_TYPE_2D = 0x00;
+const uint8_t D88_DISK_TYPE_2DD = 0x10;
+const uint8_t D88_DISK_TYPE_2HD = 0x20;
+const uint8_t D88_DISK_TYPE_1D = 0x30;
+const uint8_t D88_DISK_TYPE_1DD = 0x40;
 
 typedef struct FDT_ATTR_PACKED {
   char name[17];
-  byte reserve[9];
-  byte write_protect;
-  byte disk_type;
-  long disk_size;
-  long track_offset[164];
+  uint8_t reserve[9];
+  uint8_t write_protect;
+  uint8_t disk_type;
+  uint32_t disk_size;
+  uint32_t track_offset[164];
 } FdtD88RawHeader;
 
-const byte D88_TRACK_DELETED_MARK_NONE = 0x00;
-const byte D88_TRACK_DELETED_MARK_ENABLED = 0x10;
+const uint8_t D88_TRACK_DELETED_MARK_NONE = 0x00;
+const uint8_t D88_TRACK_DELETED_MARK_ENABLED = 0x10;
 
-const byte D88_TRACK_DENSITY_SINGLE = 0x40;
-const byte D88_TRACK_DENSITY_DOUBLE = 0x00;
-const byte D88_TRACK_DENSITY_HIGH = 0x01;
+const uint8_t D88_TRACK_DENSITY_SINGLE = 0x40;
+const uint8_t D88_TRACK_DENSITY_DOUBLE = 0x00;
+const uint8_t D88_TRACK_DENSITY_HIGH = 0x01;
 
-const byte D88_TRACK_STATUS_NORMAL = 0x00;
-const byte D88_TRACK_STATUS_NORMA_DELETED = 0x10;
-const byte D88_TRACK_STATUS_CRC_ERROR = 0xA0;
-const byte D88_TRACK_STATUS_DATS_CRC_ERROR = 0xB0;
-const byte D88_TRACK_STATUS_NON_ADDRESS_MARK = 0xE0;
-const byte D88_TRACK_STATUS_NON_DATA_MARK = 0xF0;
+const uint8_t D88_TRACK_STATUS_NORMAL = 0x00;
+const uint8_t D88_TRACK_STATUS_NORMA_DELETED = 0x10;
+const uint8_t D88_TRACK_STATUS_CRC_ERROR = 0xA0;
+const uint8_t D88_TRACK_STATUS_DATS_CRC_ERROR = 0xB0;
+const uint8_t D88_TRACK_STATUS_NON_ADDRESS_MARK = 0xE0;
+const uint8_t D88_TRACK_STATUS_NON_DATA_MARK = 0xF0;
 
 typedef struct FDT_ATTR_PACKED {
-  byte c;
-  byte h;
-  byte r;
-  byte n;
+  uint8_t c;
+  uint8_t h;
+  uint8_t r;
+  uint8_t n;
   uint8_t number_of_sector;
-  byte deleted_mark;
-  byte status;
-  byte reserve[5];
-  uint8_t size_of_data;
-  byte* data;
+  uint8_t deleted_mark;
+  uint8_t status;
+  uint8_t reserve[5];
+  uint16_t size_of_data;
 } FdtD88RawTrack;
 
 #ifdef __cplusplus
