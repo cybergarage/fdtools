3b7df4c39a2d3993af6ea00118058528b0b18646
Satoshi Konno <skonno@cybergarage.org>
2020-11-27 22:53:36 +0900

    Fix fdt_image_getsector()

--- include/fdtools/img/image.h
+++ include/fdtools/img/image.h
@@ -71,7 +71,7 @@ void fdt_image_print(FdtImage* img);
 #define fdt_image_getrpm(img) fdt_image_config_getrpm(img->config)
 #define fdt_image_getwriteprotect(img) fdt_image_config_getwriteprotect(img->config)
 
-#define fdt_image_getsector(img, c, h) fdt_image_sectors_getsector(img->sectors, c, h)
+#define fdt_image_getsector(img, c, h, r) fdt_image_sectors_getsector(img->sectors, c, h, r)
 #define fdt_image_getnumberoftracksector(img, c, h) fdt_image_sectors_getnumberoftracksector(img->sectors, c, h)
 #define fdt_image_gettracksize(img, c, h) fdt_image_sectors_gettracksize(img->sectors, c, h)
 
