fe6dfc3c18f11f5a0c03973baca37b622ec90c57
Satoshi Konno <skonno@cybergarage.org>
2020-11-30 23:26:33 +0900

    Update fdt_hfe_image_parse()

--- src/fdtools/img/hfe_loader.cpp
+++ src/fdtools/img/hfe_loader.cpp
@@ -28,6 +28,7 @@ bool fdt_hfe_image_load(FdtImage* img, FILE* fp, FdtError* err)
 
 bool fdt_hfe_image_parse(FdtImage* img, byte* header_buf)
 {
+  // TODO: Support Big-endian architecture
   FdtHfeHeader* raw_header = (FdtHfeHeader*)header_buf;
   //fdt_hfe_header_print(raw_header);
   return true;
