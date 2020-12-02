a48f07aeb84bbea7705a8f194151420e4d1de92e
Satoshi Konno <skonno@cybergarage.org>
2020-12-03 01:23:03 +0900

    Update fdt_hexdump_print()

--- src/fdtools/util/hexdump.cpp
+++ src/fdtools/util/hexdump.cpp
@@ -21,18 +21,18 @@ const size_t FDT_HEXDUMP_LINE_BYTES = 16;
 
 void fdt_hexdump_print(byte* buf, size_t buf_len)
 {
-  for (size_t offset = 0; offset < buf_len; offset+=FDT_HEXDUMP_LINE_BYTES) {
+  for (size_t offset = 0; offset < buf_len; offset += FDT_HEXDUMP_LINE_BYTES) {
     size_t line_len = FDT_HEXDUMP_LINE_BYTES;
     if (buf_len < (offset + line_len)) {
       line_len = buf_len % FDT_HEXDUMP_LINE_BYTES;
     }
     printf("%04lX ", offset);
-    for (size_t n = 0; n < (line_len/2); n++) {
-      printf("%02X", buf[offset+n] & 0xFF);
+    for (size_t n = 0; n < (line_len / 2); n++) {
+      printf("%02X", buf[offset + n] & 0xFF);
     }
     printf(" ");
-    for (size_t n = (line_len/2); n < line_len; n++) {
-      printf("%02X", buf[offset+n] & 0xFF);
+    for (size_t n = (line_len / 2); n < line_len; n++) {
+      printf("%02X", buf[offset + n] & 0xFF);
     }
     printf("\n");
   }
