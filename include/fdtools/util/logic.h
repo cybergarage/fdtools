ff1d651cd78a4a472869ad4b48d185872950b54e
Satoshi Konno <skonno@cybergarage.org>
2020-12-03 01:25:03 +0900

    Add fdt_swapbyte()

---
+++ include/fdtools/util/logic.h
@@ -0,0 +1,39 @@
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _FDTOOLS_UTIL_LOGIC_H_
+#define _FDTOOLS_UTIL_LOGIC_H_
+
+#include <fdtools/typedef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+inline byte fdt_swapbyte(uint8_t x)
+{
+  uint8_t r = 0;
+  for (int n = 0; n < 8; n++) {
+    r <<= 1;
+    r |= (x & 1);
+    x >>= 1;
+  }
+  return r;
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _FDTOOLS_UTIL_LIST_H_ */
---
+++ test/logic_test.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <boost/test/unit_test.hpp>
+
+#include <fdtools/util/array.h>
+#include <fdtools/util/logic.h>
+
+BOOST_AUTO_TEST_CASE(BitSwapTest)
+{
+  uint8_t b[] = { 0x00, 0x01, 0x02, 0x04, 0x80 };
+  uint8_t rb[] = { 0x00, 0x80, 0x40, 0xC0, 0x20 };
+
+  for (int n = 0; n < fdt_array_countof(b); n++) {
+    BOOST_CHECK_EQUAL(fdt_swapbyte(b[n]), rb[n]);
+  }
+}
