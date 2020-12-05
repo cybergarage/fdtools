095e3e1449eaab0b631b4c7d3c57700737a026e1
Satoshi Konno <skonno@cybergarage.org>
2020-12-06 02:34:41 +0900

    Update fdt_dictionary_get()

--- src/fdtools/util/dictionary.cpp
+++ src/fdtools/util/dictionary.cpp
@@ -45,7 +45,7 @@ FdtDictionaryElement* fdt_dictionary_get(FdtDictionary* dir, const char* key)
     return NULL;
 
   for (FdtDictionaryElement* elem = fdt_dictionary_gets(dir); elem != NULL; elem = fdt_dictionary_element_next(elem)) {
-    const char *elemKey = fdt_dictionary_element_getkey(elem);
+    const char* elemKey = fdt_dictionary_element_getkey(elem);
     if (fdt_strlen(elemKey) <= 0)
       continue;
     if (fdt_streq(elemKey, key) == true)
