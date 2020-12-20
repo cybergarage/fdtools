724ee16cda1aca7a1e5b0810e473a3e4bc1b3cdd
Satoshi Konno <skonno@cybergarage.org>
2020-12-20 12:20:03 +0900

    Add fdt_device_image_new()

---
+++ src/fdtools/dev/image.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+#include <string.h>
+
+#include <fdtools/img/d88.h>
+#include <fdtools/util/file.h>
+#include <fdtools/util/string.h>
+
+bool fdt_device_image_load(FdtImage*, FILE*, FdtError*);
+bool fdt_device_image_export(FdtImage*, FILE*, FdtError*);
+
+FdtImage* fdt_device_image_new(void)
+{
+  FdtImage* img = fdt_image_new();
+  if (!img)
+    return NULL;
+
+  fdt_image_setfileloader(img, fdt_device_image_load);
+  fdt_image_setfileexporter(img, fdt_device_image_export);
+
+  return img;
+}
+
+bool fdt_device_image_load(FdtImage*, FILE*, FdtError*)
+{
+  return false;
+}
+
+bool fdt_device_image_export(FdtImage*, FILE*, FdtError*)
+{
+  return false;
+}
