0c7c1044861fdcb011f597696c352e70bb79a944
Satoshi Konno <skonno@cybergarage.org>
2020-12-25 23:14:27 +0900

    Add FDT_IMAGE_CLOSER

--- include/fdtools/img/image.h
+++ include/fdtools/img/image.h
@@ -29,6 +29,7 @@ extern "C" {
 const int FDT_IMAGE_HEADER_SIGNATURE_MAX = 8;
 
 typedef bool (*FDT_IMAGE_OPENER)(void*, const char*, FdtFileMode, FdtError*);
+typedef bool (*FDT_IMAGE_CLOSER)(void*, FdtError*);
 typedef bool (*FDT_IMAGE_LOADER)(void*, FILE*, FdtError*);
 typedef bool (*FDT_IMAGE_EXPORTER)(void*, FILE*, FdtError*);
 
