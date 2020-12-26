b23ccf6c5d31ff9195298561d02e41da035618ab
Satoshi Konno <skonno@cybergarage.org>
2020-12-26 19:14:57 +0900

    Add fdt_image_file_open() and fdt_image_file_close()

--- src/fdtools/img/file.cpp
+++ src/fdtools/img/file.cpp
@@ -15,11 +15,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <fdtools/img/d88.h>
 #include <fdtools/img/file.h>
-#include <fdtools/img/hfe.h>
 #include <fdtools/util/string.h>
 
+bool fdt_image_file_open(FdtFileImage*, const char*, FdtFileMode, FdtError*);
+bool fdt_image_file_close(FdtFileImage* img, FdtError* err);
 bool fdt_image_file_delete(FdtFileImage*);
 
 FdtImage* fdt_image_file_new()
@@ -35,7 +35,9 @@ FdtImage* fdt_image_file_new()
   }
 
   img->fp = NULL;
-  
+
+  fdt_image_setopener(img, fdt_image_file_open);
+  fdt_image_setcloser(img, fdt_image_file_close);
   fdt_image_setdestructor(img, fdt_image_file_delete);
 
   return (FdtImage*)img;
@@ -43,5 +45,41 @@ FdtImage* fdt_image_file_new()
 
 bool fdt_image_file_delete(FdtFileImage* img)
 {
+  if (!img)
+    return false;
+  if (!fdt_image_clear((FdtImage*)img))
+    return false;
+  return true;
+}
+
+bool fdt_image_file_open(FdtFileImage* img, const char* name, FdtFileMode mode, FdtError* err)
+{
+  if (!img)
+    return false;
+
+  img->fp = fdt_file_open(name, mode);
+  if (!img->fp) {
+    fdt_error_setlasterror(err, "");
+    return false;
+  }
+
+  return true;
+}
+
+bool fdt_image_file_close(FdtFileImage* img, FdtError* err)
+{
+  if (!img)
+    return false;
+
+  if (!img->fp)
+    return true;
+
+  if (!fdt_file_close(img->fp)) {
+    fdt_error_setlasterror(err, "");
+    return false;
+  }
+
+  img->fp = NULL;
+
   return true;
 }
