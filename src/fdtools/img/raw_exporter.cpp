6d9f021eb04c8366df163b4cd516ff46e1f2dc11
Satoshi Konno <skonno@cybergarage.org>
2020-12-27 23:47:27 +0900

    Update fdt_raw_image_export()

--- src/fdtools/img/raw_exporter.cpp
+++ src/fdtools/img/raw_exporter.cpp
@@ -21,9 +21,34 @@
 
 bool fdt_raw_image_export(FdtFileImage* img, FdtError* err)
 {
+  if (!fdt_image_file_isvalid(img, err))
+    return false;
+
   FILE* fp = fdt_image_file_getfile(img);
   if (!fp)
     return false;
+
+  FdtImageConfig* config = fdt_image_getconfig(img);
+  if (!config)
+    return false;
+
+  size_t number_of_cylinder = fdt_image_config_getnumberofcylinder(config);
+  size_t number_of_head = fdt_image_config_getnumberofhead(config);
+  size_t number_of_sector = fdt_image_config_getnumberofsector(config);
+
+  for (size_t c = 0; c < number_of_cylinder; c++) {
+    for (size_t h = 0; h < number_of_head; h++) {
+      for (size_t s = 0; s < number_of_sector; s++) {
+        FdtImageSector* sector = fdt_image_getsector(img, c, h, s);
+        if (!sector)
+          return false;
+        if (!fdt_file_write(fp, fdt_image_sector_getdata(sector), fdt_image_sector_getsize(sector))) {
+          fdt_error_setlasterror(err, "");
+          return false;
+        }
+      }
+    }
+  }
 
   return true;
 }
