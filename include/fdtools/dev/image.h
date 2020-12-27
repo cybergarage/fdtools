ac09036c7dcdb4a2bd528b9fbba670a2e5529637
Satoshi Konno <skonno@cybergarage.org>
2020-12-27 17:45:43 +0900

    Add fdt_device_image_getsectors()

--- include/fdtools/dev/image.h
+++ include/fdtools/dev/image.h
@@ -32,7 +32,8 @@ FdtImage* fdt_device_image_new(void);
 #define fdt_device_image_isopened(img) fdt_device_isopened(img->dev)
 #define fdt_device_image_generatesectors(img) fdt_image_generatesectors((FdtImage*)img)
 #define fdt_device_image_getsectoroffset(img, sector) fdt_image_getsectoroffset((FdtImage*)img, sector)
-#define fdt_device_image_getsector(img, , c, h, n) fdt_image_getsector((FdtImage*)img, c, h, n)
+#define fdt_device_image_getsectors(img) fdt_image_getsectors((FdtImage*)img)
+#define fdt_device_image_getsector(img, c, h, n) fdt_image_getsector((FdtImage*)img, c, h, n)
 
 #ifdef __cplusplus
 } /* extern C */
