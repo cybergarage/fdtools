3a5bac563f6fa3ece0a90120f4b6113627bf9e3c
Satoshi Konno <skonno@cybergarage.org>
2020-12-29 22:13:29 +0900

    Update RAWImageLoaderTest()

--- test/raw_test.cpp
+++ test/raw_test.cpp
@@ -16,14 +16,65 @@
 #include <boost/test/unit_test.hpp>
 
 #include <fdtools/img/raw.h>
+#include <fdtools/img/file.h>
+#include <fdtools/util/array.h>
 
-BOOST_AUTO_TEST_CASE(RawLoaderTest)
+const std::string TEST_IMAGE_DIRECTORY = "./img";
+
+BOOST_AUTO_TEST_CASE(RAWImageLoaderTest)
 {
+  const char TEST_RAW_IMAGES[][64] = {
+    "test-001.raw",
+    "test-001.img",
+  };
+
   FdtError* err = fdt_error_new();
 
-  FdtImage* src_img = fdt_raw_image_new();
-  BOOST_CHECK(src_img);
-  BOOST_CHECK(fdt_image_close(src_img, err));
+  for (int n = 0; n < fdt_array_countof(TEST_RAW_IMAGES); n++) {
+    BOOST_CHECK_EQUAL(fdt_image_name_gettype(TEST_RAW_IMAGES[n]), FDT_IMAGE_TYPE_RAW);
+    
+    std::string filename = TEST_IMAGE_DIRECTORY + "/" + TEST_RAW_IMAGES[n];
+    boost::filesystem::path filepath(filename);
+    if (!boost::filesystem::exists(filepath))
+      continue;
+
+    BOOST_CHECK_EQUAL(fdt_image_name_gettype(filename.c_str()), FDT_IMAGE_TYPE_RAW);
+
+    // Loader test
+
+    FdtImage* src_img = fdt_raw_image_new();
+    BOOST_CHECK(src_img);
+    BOOST_CHECK(fdt_image_open(src_img, filename.c_str(), FDT_FILE_READ, err));
+    BOOST_CHECK(fdt_image_load(src_img, err));
+    //fdt_image_print(src_img);
+    BOOST_CHECK(fdt_image_close(src_img, err));
+
+    // Exporter test
+
+    size_t img_size = fdt_image_getsize(src_img);
+
+    byte_t* dst_img_buf = (byte_t*)malloc(img_size);
+    FILE* mem_fp = fdt_file_memopen(dst_img_buf, img_size, FDT_FILE_WRITE);
+    fdt_image_file_setfile(src_img, mem_fp);
+    BOOST_CHECK(fdt_image_export(src_img, err));
+    BOOST_CHECK(fdt_image_close(src_img, err));
+
+    // Compare test
+
+    mem_fp = fdt_file_memopen(dst_img_buf, img_size, FDT_FILE_READ);
+    FdtImage* dst_img = fdt_raw_image_new();
+    fdt_image_file_setfile(dst_img, mem_fp);
+    BOOST_CHECK(fdt_image_load(dst_img, err));
+    BOOST_CHECK(fdt_image_close(dst_img, err));
+    BOOST_CHECK(fdt_image_equals(src_img, dst_img));
+
+    free(dst_img_buf);
+
+    // Clean up
+
+    fdt_image_delete(dst_img);
+    fdt_image_delete(src_img);
+  }
 
   fdt_error_delete(err);
 }
