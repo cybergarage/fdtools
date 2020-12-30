54970b24d6febe8abf1577e95a4f8d5d5a820f69
Satoshi Konno <skonno@cybergarage.org>
2020-12-31 01:19:00 +0900

    Add fdt_image_setopenchecker()

--- include/fdtools/img/image.h
+++ include/fdtools/img/image.h
@@ -36,21 +36,23 @@ typedef enum {
 
 const int FDT_IMAGE_HEADER_SIGNATURE_MAX = 8;
 
-#define FDT_IMAGE_STRUCT_MEMBERS     \
-  FdtImageType type;                 \
-  FdtImageConfig* config;            \
-  FdtImageSectors* sectors;          \
-  FDT_IMAGE_OPENER image_opener;     \
-  FDT_IMAGE_CLOSER image_closer;     \
-  FDT_IMAGE_LOADER image_loader;     \
-  FDT_IMAGE_EXPORTER image_exporter; \
-  FDT_IMAGE_DESTRUCTOR image_destructor;
-
 typedef bool (*FDT_IMAGE_OPENER)(void*, const char*, FdtFileMode, FdtError*);
 typedef bool (*FDT_IMAGE_CLOSER)(void*, FdtError*);
+typedef bool (*FDT_IMAGE_OPENCHECKER)(void*);
 typedef bool (*FDT_IMAGE_LOADER)(void*, FdtError*);
 typedef bool (*FDT_IMAGE_EXPORTER)(void*, FdtError*);
 typedef bool (*FDT_IMAGE_DESTRUCTOR)(void*);
+
+#define FDT_IMAGE_STRUCT_MEMBERS          \
+  FdtImageType type;                      \
+  FdtImageConfig* config;                 \
+  FdtImageSectors* sectors;               \
+  FDT_IMAGE_OPENER image_opener;          \
+  FDT_IMAGE_CLOSER image_closer;          \
+  FDT_IMAGE_OPENCHECKER image_openchcker; \
+  FDT_IMAGE_LOADER image_loader;          \
+  FDT_IMAGE_EXPORTER image_exporter;      \
+  FDT_IMAGE_DESTRUCTOR image_destructor;
 
 typedef struct FDT_ATTR_PACKED {
   FDT_IMAGE_STRUCT_MEMBERS
@@ -64,6 +66,7 @@ bool fdt_image_clear(FdtImage*);
 
 bool fdt_image_open(FdtImage*, const char*, FdtFileMode, FdtError*);
 bool fdt_image_close(FdtImage*, FdtError*);
+bool fdt_image_isopened(FdtImage*);
 bool fdt_image_load(FdtImage*, FdtError*);
 bool fdt_image_import(FdtImage*, FdtImage*, FdtError*);
 bool fdt_image_export(FdtImage*, FdtError*);
@@ -79,6 +82,7 @@ void fdt_image_print(FdtImage* img);
 
 #define fdt_image_setopener(img, fn) (img->image_opener = (FDT_IMAGE_OPENER)fn)
 #define fdt_image_setcloser(img, fn) (img->image_closer = (FDT_IMAGE_CLOSER)fn)
+#define fdt_image_setopenchecker(img, fn) (img->image_openchcker = (FDT_IMAGE_OPENCHECKER)fn)
 #define fdt_image_setloader(img, fn) (img->image_loader = (FDT_IMAGE_LOADER)fn)
 #define fdt_image_setexporter(img, fn) (img->image_exporter = (FDT_IMAGE_EXPORTER)fn)
 #define fdt_image_setdestructor(img, fn) (img->image_destructor = (FDT_IMAGE_DESTRUCTOR)fn)
