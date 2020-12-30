f5ea418e7dd936d6d8eea7033b0e237f04bbb674
Satoshi Konno <skonno@cybergarage.org>
2020-12-30 18:16:24 +0900

    Update fddump

--- bin/fddump/main.c
+++ bin/fddump/main.c
@@ -12,8 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stdio.h>
 #include <stdlib.h>
+
+#include <fdtools/util/program.h>
+
+void print_usage(FdtProgram* prg)
+{
+  printf("Usage: %s\n", fdt_program_getname(prg));
+  fdt_program_printoptionusages(prg);
+}
+
+void print_error(FdtError* err)
+{
+  printf("Error: %s\n", fdt_error_getmessage(err));
+}
+
 int main(int argc, char* argv[])
 {
+  FdtError* err = fdt_error_new();
+  if (!err)
+    return EXIT_FAILURE;
+
+  FdtProgram* prg = fdt_program_new();
+  if (!prg)
+    return EXIT_FAILURE;
+  fdt_program_addoption(prg, "c", "", true);
+  fdt_program_addoption(prg, "h", "", true);
+  fdt_program_addoption(prg, "s", "", true);
+  fdt_program_addoption(prg, "v", "", false);
+
+  if (!fdt_program_parse(prg, argc, argv, err)) {
+    print_error(err);
+    print_usage(prg);
+    return EXIT_FAILURE;
+  }
+
+  if (fdt_program_getnarguments(prg) < 2) {
+    print_usage(prg);
+    return EXIT_FAILURE;
+  }
+
+  fdt_program_delete(prg);
+  fdt_error_delete(err);
+
   return 0;
 }
