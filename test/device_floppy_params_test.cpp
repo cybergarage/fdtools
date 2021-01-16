9a1faf1a527ab9c54a19567610709e6681b02b5e
Satoshi Konno <skonno@cybergarage.org>
2021-01-16 22:14:17 +0900

    Add FloppySet35InchParamsTest()

--- test/device_floppy_params_test.cpp
+++ test/device_floppy_params_test.cpp
@@ -24,10 +24,27 @@ BOOST_AUTO_TEST_CASE(FloppySet525InchParamsTest)
   // getfdprm parameters
   // DS HD tpi=96 size=2400 sect=15 dtr=0 perp=0 fm=0 cyl=80 ssize=512 gap=0x1b fmt_gap=0x6c spec1=0x00 (AT/HD)
   // DS HD tpi=96 size=2464 sect=8 dtr=0 perp=0 fm=0 cyl=77 ssize=1KB gap=0x1b fmt_gap=0x6c spec1=0x00  (98/HD)
-  // DS DD tpi=48 size=640 sect=16 dtr=1 perp=0 fm=0 cyl=40 ssize=256 gap=0x23 fmt_gap=0x46 spec1=0x00 (X68/HD)
+  // DS DD tpi=48 size=640 sect=16 dtr=1DD perp=0 fm=0 cyl=40 ssize=256 gap=0x23 fmt_gap=0x46 spec1=0x00 (X68/HD)
   // DS DD tpi=48 size=640 sect=16 dtr=1 perp=0 fm=0 cyl=40 ssize=256 gap=0x23 fmt_gap=0x46 spec1=0x00 (X1,88/2D)
   // DS HD tpi=96 size=2080 sect=26 dtr=0 perp=0 fm=0 cyl=80 ssize=256 gap=0x1b fmt_gap=0x50 spec1=0x00 (88/HD)
-  // DS HD tpi=96 size=1232 sect=16 dtr=0 perp=0 fm=0 cyl=77 ssize=256 gap=0x1b fmt_gap=0x6c spec1=0x00 (88/X1)
+  // DS HD tpi=96 size=1232 sect=16 dtr=0 perp=0 fm=0 cyl=77 ssize=256 gap=0x1b fmt_gap=0x6c spec1=0x00 (X1/HD)
+  FdtError* err = fdt_error_new();
+  BOOST_REQUIRE(err);
+
+  BOOST_CHECK(fdt_error_delete(err));
+}
+
+BOOST_AUTO_TEST_CASE(FloppySet35InchParamsTest)
+{
+  // getfdprm parameters
+  // DS HD stretch=0 size=2880 sect=18 dtr=0 perp=0 fm=0 cyl=80 ssize=512 gap=0x1b fmt_gap=0x6c spec1=0x00 (AT/HD)
+  // DS HD stretch=0 size=2464 sect=8 dtr=0 perp=0 fm=0 cyl=77 ssize=1KB gap=0x1b fmt_gap=0x6c spec1=0x00  (98/HD)
+  // DS HD stretch=0 size=2880 sect=18 dtr=0 perp=0 fm=0 cyl=80 ssize=512 gap=0x1b fmt_gap=0x6c spec1=0xcf (FM/2D)
+  // DS DD stretch=0 size=1280 sect=16 dtr=2 perp=0 fm=0 cyl=80 ssize=256 gap=0x2a fmt_gap=0x46 spec1=0x00 (FM/2DD)
+  // DS DD stretch=0 size=1440 sect=9 dtr=2 perp=0 fm=0 cyl=80 ssize=512 gap=0x2a fmt_gap=0x6c spec1=0x00 (MSX/2DD)
+  // SS DD stretch=0 size=720 sect=9 dtr=2 perp=0 fm=0 cyl=80 ssize=512 gap=0x2a fmt_gap=0x6c spec1=0x00 (MSX/1DD)
+  // DS DD stretch=0 size=1280 sect=16 dtr=2 perp=0 fm=0 cyl=80 ssize=256 gap=0x2a fmt_gap=0x46 spec1=0x00 (MZ/2DD)
+  // SS DD stretch=0 size=560 sect=16 dtr=2 perp=0 fm=0 cyl=70 ssize=256 gap=0x2a fmt_gap=0x46 spec1=0x00  (SMC777/1DD)
   FdtError* err = fdt_error_new();
   BOOST_REQUIRE(err);
 
