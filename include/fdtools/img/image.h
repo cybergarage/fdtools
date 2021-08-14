729c75c7e631fc2f9420349978e08bf2bea862d0
Satoshi Konno <skonno@cybergarage.org>
2021-08-14 14:37:26 +0900

    Rename fdt_image_*configname() to fdt_image_*name()

--- include/fdtools/img/image.h
+++ include/fdtools/img/image.h
@@ -107,7 +107,7 @@ void fdt_image_print(FdtImage* img);
 #define fdt_image_setdestructor(img, fn) (img->image_destructor = (FDT_IMAGE_DESTRUCTOR)fn)
 
 #define fdt_image_settarget(img, v) fdt_string_setvalue(img->target, v)
-#define fdt_image_setconfigname(img, v) fdt_image_config_setname(img->config, v)
+#define fdt_image_setname(img, v) fdt_image_config_setname(img->config, v)
 #define fdt_image_setsize(img, v) fdt_image_config_setsize(img->config, v)
 #define fdt_image_setdensity(img, v) fdt_image_config_setdensity(img->config, v)
 #define fdt_image_setnumberofhead(img, v) fdt_image_config_setnumberofhead(img->config, v)
@@ -118,10 +118,9 @@ void fdt_image_print(FdtImage* img);
 #define fdt_image_setwriteprotect(img, v) fdt_image_config_setwriteprotect(img->config, v)
 
 #define fdt_image_isdevice(img) ((fdt_image_gettype(img) == FDT_IMAGE_TYPE_DEV) ? true : false)
-#define fdt_image_hasname(img) ((0 < fdt_string_length(img->target)) ? true : false)
 #define fdt_image_gettarget(img) fdt_string_getvalue(img->target)
-#define fdt_image_hasconfigname(img) fdt_image_config_hasname(img->config)
-#define fdt_image_getconfigname(img) fdt_image_config_getname(img->config)
+#define fdt_image_hasname(img) fdt_image_config_hasname(img->config)
+#define fdt_image_getname(img) fdt_image_config_getname(img->config)
 #define fdt_image_getsize(img) fdt_image_config_getsize(img->config)
 #define fdt_image_getdensity(img) fdt_image_config_getdensity(img->config)
 #define fdt_image_getsupposeddensity(img) fdt_image_config_getsupposeddensity(img->config)
