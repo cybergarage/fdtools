ba0265ded46b01823ba853c7f4b0068985809608
Satoshi Konno <skonno@cybergarage.org>
2021-09-09 23:22:10 +0900

    Update fdt_device_detecttransferrate()

--- src/fdtools/dev/device_linux.cpp
+++ src/fdtools/dev/device_linux.cpp
@@ -110,14 +110,55 @@ bool fdt_device_getfloppyparameters(FdtDevice* dev, FdtFloppyParams* params, Fdt
   return is_success;
 }
 
-bool fdt_device_detecttransferrate(int fd, FdtFloppyParams* params, FdtError* err)
+bool fdt_device_detecttransferratewithrate(int fd, int rate, FdtFloppyParams* params, FdtError* err)
 {
+  // See: https://fdutils.linux.lu/disk-id.html
+  // Finding out the data transfer rate - How to identify an unknown disk using Fdutils
+  // First, you need to find out the data transfer rate (roughly the density: high, double, single).
+  // For this, you try out fdrawcmd readid 0 rate=x, where x is 0, 1, 2, 3 If no rate is supplied, fdrawcmd assumes rate=0
+
   floppy_raw_cmd raw_cmd;
-  if (!fdt_floppy_rawcmd_readid(fd, fdt_floppy_params_getdeviceno(params), 0, 0, &raw_cmd))
+  if (!fdt_floppy_rawcmd_readid(fd, fdt_floppy_params_getdeviceno(params), rate, 0, &raw_cmd))
     return false;
 
   fdt_floppy_rawcmd_print(&raw_cmd);
+
+  if (raw_cmd->reply_count < 7)
+    return false;
+
+  // The first 3 output bytes are error codes. The first byte should be below 7 (i.e. the example above succeeded).
+  // The last four bytes is a sector header.
+  // Byte 3 is the cylinder (here: 0), byte 4 is the head (here: 0).
+  // Byte 5 is the sector number (here: 1) of the sector which happened to be near the R/W head when the command was issued.
+  // Byte 6 is the sector size code. Size code 0 means 128 bytes per sector, size code 1 means 256 bytes per sector, size code 2 means 512 bytes per sector, etc (i.e. size doubles with each increment).
+  // So, you try out all 4 rates until you get one that succeeds (byte 0 <= 7). If none matches, try adding the FM keyword (to use FM coding instead of MFM).
+
   return true;
+}
+
+bool fdt_device_detecttransferrate(int fd, FdtFloppyParams* params, FdtError* err)
+{
+  // See: http://man.he.net/man1/fdrawcmd
+  // fdrawcmd
+  // rate=rate
+  // Selects  the data transfer rate. Use 0 for high density disks, 1
+  // for double density 5 1/4 disks (or 2 Mbps tapes, if the  appropriate rate table is selected),
+  // and 2 for double density 3 1/2 disks.
+
+  int rates = {
+    0x000,
+    0x001,
+    0x002,
+    0x043,
+    0x100,
+  };
+
+  for (int n = 0; n < 4; n++) {
+    if (fdt_device_detecttransferratewithrate(fd, rates[n], params, err))
+      return true;
+  }
+
+  return false
 }
 
 bool fdt_device_detectfloppyformat(FdtDevice* dev, FdtFloppyParams* params, FdtError* err)
