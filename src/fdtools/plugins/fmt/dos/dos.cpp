acfb68521bc914beba137d749b66518968e15a6b
Satoshi Konno <skonno@cybergarage.org>
2022-09-17 11:17:35 +0900

    Update fdt_dos_format_new()

--- src/fdtools/plugins/fmt/dos/dos.cpp
+++ src/fdtools/plugins/fmt/dos/dos.cpp
@@ -16,16 +16,17 @@
 
 FdtFormat* fdt_dos_format_new(void)
 {
-  FdtFormat* img = fdt_format_new();
-  if (!img)
+  FdtFormat* fmt = fdt_format_new();
+  if (!fmt)
     return NULL;
 
-  fdt_format_setgettypeid(img, fdt_dos_format_gettypeid);
+  fmt->type = FDT_FORMAT_TYPE_DOS;
+  fdt_format_setgettypeid(fmt, fdt_dos_format_gettypeid);
 
-  return img;
+  return fmt;
 }
 
-const char* fdt_dos_format_gettypeid(FdtFormat* img)
+const char* fdt_dos_format_gettypeid(FdtFormat* fmt)
 {
   return "DOS";
 }
