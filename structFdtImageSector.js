2b57b88db51346aeb17e8df44ac6d543e1fc5ac0
cybergarage <cybergarage@users.noreply.github.com>
2022-09-19 09:38:23 +0000

    deploy: 5988e4bb667cf6e66a0ce955eef82a29c47af289

--- structFdtImageSector.js
+++ structFdtImageSector.js
@@ -1,9 +1,9 @@
 var structFdtImageSector =
 [
+    [ "cylinder_number", "structFdtImageSector.html#a3408b3502a40819c840c233544197cca", null ],
     [ "data", "structFdtImageSector.html#ad501fe9ff09be2880bc8f02c2a55dba5", null ],
     [ "error_count", "structFdtImageSector.html#acdbf576eaad4d46de6368f2bb002c48b", null ],
     [ "head_number", "structFdtImageSector.html#ab9bf1b7c4d4bc3d6dc71bc6d3a68ad03", null ],
     [ "number", "structFdtImageSector.html#aea4272851cc4b85a748fcbfa919dc86a", null ],
-    [ "size", "structFdtImageSector.html#a8ff97f3f48ea894c06e6018a12597fb8", null ],
-    [ "track_number", "structFdtImageSector.html#abc7327fd45a0fe80e0234324e26afbbb", null ]
+    [ "size", "structFdtImageSector.html#a8ff97f3f48ea894c06e6018a12597fb8", null ]
 ];
