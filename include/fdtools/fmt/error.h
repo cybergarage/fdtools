673060e06d3cbd4cb16fc9c510912b84e97f8e28
Satoshi Konno <skonno@cybergarage.org>
2022-09-21 23:08:59 +0900

    Add error.h

---
+++ include/fdtools/fmt/error.h
@@ -0,0 +1,31 @@
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _FDTOOLS_FMT_ERROR_H_
+#define _FDTOOLS_FMT_ERROR_H_
+
+#include <fdtools/error.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FDT_FORMAT_ERROR_NO_IMAGE "Image is not found"
+#define FDT_FORMAT_ERROR_INVALID_OPERATION "Operation (%s) is not supported"
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* _FDTOOLS_FMT_ERROR_H_ */
