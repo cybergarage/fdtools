be35a5c5c72e23a9a908d208a5f5e31a84ced36f
Satoshi Konno <skonno@cybergarage.org>
2022-09-22 23:03:19 +0900

    Update ImagePluginTest

--- test/image_plugin_test.cpp
+++ test/image_plugin_test.cpp
@@ -20,38 +20,51 @@
 BOOST_AUTO_TEST_CASE(ImagePluginTest)
 {
   FdtError* err = fdt_error_new();
-  BOOST_REQUIRE(err);
-  boost::filesystem::path test_img_path(TEST_IMAGE_DIRECTORY);
-  boost::filesystem::recursive_directory_iterator end;
-  for (boost::filesystem::recursive_directory_iterator it(test_img_path); it != end; ++it) {
-    const boost::filesystem::path test_img_filepath = (*it);
-    if (!boost::filesystem::exists(test_img_filepath))
-      continue;
+  FdtStrings* all_target_exts = fdt_strings_new();
+  // fdt_image_plugins_getallextentions(all_target_exts);
+  fdt_strings_addcstring(all_target_exts, "HFE");
 
-    const boost::filesystem::path test_img_filename = test_img_filepath.filename();
+  for (FdtString* target_ext = fdt_strings_gets(all_target_exts); target_ext; target_ext = fdt_string_next(target_ext)) {
 
-    FDT_IMAGE_IMAGER test_imager = fdt_image_plugins_getimager(test_img_filepath.c_str(), err);
-    if (!test_imager)
-      continue;
+    boost::filesystem::path test_img_path(TEST_IMAGE_DIRECTORY);
+    boost::filesystem::recursive_directory_iterator end;
 
-    /*
-    // Loads a found test disk image
+    for (boost::filesystem::recursive_directory_iterator it(test_img_path); it != end; ++it) {
+      const boost::filesystem::path test_img_filepath = (*it);
+      if (!boost::filesystem::exists(test_img_filepath))
+        continue;
 
-    BOOST_TEST_MESSAGE("Loading   <- " << test_img_filename);
+      const boost::filesystem::path test_img_ext = test_img_filepath.extension();
+      std::string target_dot_ext = ".";
+      target_dot_ext += fdt_string_getvalue(target_ext);
+      if (fdt_strcasecmp(test_img_ext.c_str(), target_dot_ext.c_str()) != 0)
+        continue;
 
-    FdtImage* test_img = test_imager();
-    BOOST_REQUIRE(test_img);
-    fdt_image_settarget(test_img, test_img_filepath.c_str());
-    BOOST_REQUIRE_MESSAGE(fdt_image_load(test_img, err), fdt_error_getdebugmessage(err));
+      const boost::filesystem::path test_img_filename = test_img_filepath.filename();
+      FDT_IMAGE_IMAGER test_imager = fdt_image_plugins_getimager(test_img_filepath.c_str(), err);
+      if (!test_imager)
+        continue;
 
-    BOOST_TEST_MESSAGE("Loaded    <- " << test_img_filename << "(" << fdt_image_gettypeid(test_img) << ")");
+      // Loads a found test disk image
 
-    // Exports the loaded image to a memory file, and compare the exported image with the original image
+      BOOST_TEST_MESSAGE("Loading   <- " << test_img_filename);
 
-    ImageExportCompareTest(test_img, test_imager);
+      FdtImage* test_img = test_imager();
+      BOOST_REQUIRE(test_img);
+      fdt_image_settarget(test_img, test_img_filepath.c_str());
+      BOOST_REQUIRE_MESSAGE(fdt_image_load(test_img, err), fdt_error_getdebugmessage(err));
 
-    BOOST_CHECK(fdt_image_delete(test_img));
-     */
+      BOOST_TEST_MESSAGE("Loaded    <- " << test_img_filename << "(" << fdt_image_gettypeid(test_img) << ")");
+
+      /*
+       // Exports the loaded image to a memory file, and compare the exported image with the original image
+       ImageExportCompareTest(test_img, test_imager);
+       */
+
+      BOOST_CHECK(fdt_image_delete(test_img));
+    }
   }
-  BOOST_CHECK(fdt_error_delete(err));
+
+  fdt_strings_delete(all_target_exts);
+  fdt_error_delete(err);
 }
