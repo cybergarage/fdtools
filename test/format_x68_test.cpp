97d3902f4dfb193b3c7e0886fd0fef4b09b2986b
Satoshi Konno <skonno@cybergarage.org>
2023-10-13 09:53:02 +0900

    Update FormatX68Test()

--- test/format_x68_test.cpp
+++ test/format_x68_test.cpp
@@ -15,12 +15,22 @@
 #include <boost/test/unit_test.hpp>
 #include <fdtools/plugins/fmt/x68/x68.h>
 
+const std::string TEST_X86_BLANK_IMAGE = "./img/525-2hd-x68.xdf";
+
 BOOST_AUTO_TEST_CASE(FormatX68Test)
 {
+  FdtError* err = fdt_error_new();
+
   FdtImage* img = fdt_x68_floppy_image_new();
   BOOST_CHECK(img);
   BOOST_CHECK(fdt_x68_image_isfloppy(img));
   BOOST_CHECK(fdt_x68_image_generate(img));
   BOOST_CHECK(fdt_x68_image_isfloppy(img));
   BOOST_CHECK(fdt_x68_image_delete(img));
+
+   FdtImage* org_img = fdt_x68_floppy_image_from(TEST_X86_BLANK_IMAGE.c_str(), err);
+  BOOST_CHECK(org_img);
+  BOOST_CHECK(fdt_x68_image_delete(org_img));
+
+  fdt_error_delete(err);
 }
