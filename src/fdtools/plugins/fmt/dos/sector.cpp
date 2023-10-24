a27b727582fecf324d9901de36fcb59c4252a480
Satoshi Konno <skonno@cybergarage.org>
2023-10-25 08:57:26 +0900

    Update fdt_dos_formatbootsector()

--- src/fdtools/plugins/fmt/dos/sector.cpp
+++ src/fdtools/plugins/fmt/dos/sector.cpp
@@ -16,6 +16,9 @@
 #include <fdtools/plugins/fmt/dos/dos.h>
 #include <fdtools/plugins/fmt/dos/sector.h>
 
+const byte_t FDT_DOS_FAT_BPB_JUMPBOOT[3] = { 0xEB, 0x3C, 0x90 };
+const byte_t FDT_DOS_FAT_BPB_OEMNAME[8] = { 'M', 'S', 'D', 'O', 'S', '5', '.', '0' };
+
 bool fdt_dos_formatbootsector(FdtFormat* fmt, FdtError* err)
 {
   FdtImage* img = fdt_format_getimage(fmt);
@@ -34,6 +37,9 @@ bool fdt_dos_formatbootsector(FdtFormat* fmt, FdtError* err)
   }
 
   FdtFatBpb* bpb = fdt_fat_bpb_from(fdt_image_sector_getdata(boot_sector));
+
+  fdt_fat_bpb_setjumpboot(bpb, FDT_DOS_FAT_BPB_JUMPBOOT);
+  fdt_fat_bpb_setoemname(bpb, FDT_DOS_FAT_BPB_OEMNAME);
 
   return true;
 }
