7f495285a9dbcc1e3b3cebff753efa7c40036aca
Satoshi Konno <skonno@cybergarage.org>
2023-10-24 21:03:46 +0900

    Update Dos35FormatTest()

--- test/dos_test.cpp
+++ test/dos_test.cpp
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 #include <boost/test/unit_test.hpp>
-#include <fdtools/plugins/fmt/dos/fat.h>
+#include <fdtools/img/image.h>
+#include <fdtools/plugins/fmt/dos/dos.h>
 #include <fdtools/plugins/image.h>
-
-const std::string TEST_DOS_BLANK_IMAGE = "./img/35-2hd-msdos.raw";
+#include <fdtools/util/hexdump.h>
 
 BOOST_AUTO_TEST_CASE(DosFatTest)
 {
@@ -24,27 +24,55 @@ BOOST_AUTO_TEST_CASE(DosFatTest)
   BOOST_CHECK_EQUAL(sizeof(FdtFatBpbExtended), (512 - sizeof(FdtFatBpb)));
 }
 
-BOOST_AUTO_TEST_CASE(DosFormatTest)
+BOOST_AUTO_TEST_CASE(Dos35FormatTest)
 {
+  const std::string TEST_DOS_BLANK_IMAGE = "./img/35-2hd-msdos.raw";
+
   FdtError* err = fdt_error_new();
 
-  //  FdtImage* img = fdt_x68_floppy_image_new();
-  //  BOOST_REQUIRE(img);
-  //  BOOST_REQUIRE(fdt_x68_image_isfloppy(img));
-  //  BOOST_REQUIRE(fdt_x68_image_generate(img));
-  //  BOOST_REQUIRE(fdt_x68_image_isfloppy(img));
+  FdtImage* img = fdt_image_new();
+  BOOST_REQUIRE(img);
+  fdt_image_setdensity(img, FDT_IMAGE_DENSITY_HD);
+  fdt_image_setnumberofcylinder(img, FDT_DOS_35_FD_CYLINDER);
+  fdt_image_setnumberofhead(img, FDT_DOS_35_FD_HEAD);
+  fdt_image_setnumberofsector(img, FDT_DOS_35_FD_SECTOR);
+  fdt_image_setsectorsize(img, FDT_DOS_35_FD_SECTOR_SIZE);
+  BOOST_REQUIRE(fdt_image_generatesectors(img, err));
 
-  //  FdtFormat* fmt = fdt_x68_from(img);
-  //  BOOST_REQUIRE(img);
-  //  BOOST_REQUIRE_MESSAGE(fdt_format_format(fmt, err), fdt_error_getmessage(err));
-  //  BOOST_CHECK(fdt_format_delete(fmt));
+  FdtFormat* fmt = fdt_dos_from(img, err);
+  BOOST_REQUIRE(img);
+  BOOST_REQUIRE_MESSAGE(fdt_format_format(fmt, err), fdt_error_getmessage(err));
+  BOOST_CHECK(fdt_format_delete(fmt));
 
   FdtImage* org_img = fdt_image_from(TEST_DOS_BLANK_IMAGE.c_str(), err);
   BOOST_REQUIRE_MESSAGE(org_img, fdt_error_getmessage(err));
 
-  //  BOOST_REQUIRE_MESSAGE(fdt_image_equals(img, org_img, err), fdt_error_getmessage(err));
+  BOOST_CHECK_MESSAGE(fdt_image_equals(img, org_img, err), fdt_error_getmessage(err));
+
+  for (FdtImageSector* sector = fdt_image_sectors_gets(org_img->sectors); sector; sector = fdt_image_sector_next(sector)) {
+    size_t t = fdt_image_sector_getcylindernumber(sector);
+    size_t h = fdt_image_sector_getheadnumber(sector);
+    size_t s = fdt_image_sector_getnumber(sector);
+    FdtImageSector* other = fdt_image_sectors_getsector(img->sectors, t, h, s);
+    if (!other) {
+      continue;
+    }
+
+    if (fdt_image_sector_equals(sector, other, err))
+      continue;
+
+    byte_t* sector_l_data = fdt_image_sector_getdata(sector);
+    size_t sector_l_size = fdt_image_sector_getsize(sector);
+
+    byte_t* sector_r_data = fdt_image_sector_getdata(other);
+    size_t sector_r_size = fdt_image_sector_getsize(other);
+
+    fdt_hexdump_compare_print(sector_l_data, sector_l_size, sector_r_data, sector_r_size);
+
+    break;
+  }
 
   BOOST_CHECK(fdt_error_delete(err));
-  //  BOOST_CHECK(fdt_x68_image_delete(img));
-  //  BOOST_CHECK(fdt_x68_image_delete(org_img));
+  BOOST_CHECK(fdt_image_delete(img));
+  BOOST_CHECK(fdt_image_delete(org_img));
 }
