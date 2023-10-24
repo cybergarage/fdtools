9dd4aeae0df71aa7ceaedd08ffd4594fe883bc2a
Satoshi Konno <skonno@cybergarage.org>
2023-10-24 23:21:53 +0900

    Add fdt_dos_formatbootsector()

---
+++ src/fdtools/plugins/fmt/dos/sector.h
@@ -0,0 +1,31 @@
+
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _FDTOOLS_DOS_SECTOR_H_
+#define _FDTOOLS_DOS_SECTOR_H_
+
+#include <fdtools/fmt/format.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool fdt_dos_formatbootsector(FdtFormat*, FdtError*);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* _FDTOOLS_DOS_SECTOR_H_ */
