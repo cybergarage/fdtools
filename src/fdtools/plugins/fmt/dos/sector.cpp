4743f1f53b015acfaa82c8269aa3c22607594767
Satoshi Konno <skonno@cybergarage.org>
2023-10-25 20:23:44 +0900

    Update fdt_dos_formatbootsector()

--- src/fdtools/plugins/fmt/dos/sector.cpp
+++ src/fdtools/plugins/fmt/dos/sector.cpp
@@ -40,7 +40,16 @@ bool fdt_dos_formatbootsector(FdtFormat* fmt, FdtError* err)
 
   fdt_fat_bpb_setjumpboot(bpb, FDT_DOS_FAT_BPB_JUMPBOOT);
   fdt_fat_bpb_setoemname(bpb, FDT_DOS_FAT_BPB_OEMNAME);
+  fdt_fat_bpb_setsecperclus(bpb, 1);
+  fdt_fat_bpb_setrsvdseccnt(bpb, 1);
+  fdt_fat_bpb_setnumfats(bpb, 2);
+  fdt_fat_bpb_setrootentcnt(bpb, 224);
+  fdt_fat_bpb_setmedia(bpb, 0xF0);
+  size_t n = fdt_image_getnsectors(img);
+  fdt_fat_bpb_settotsec16(bpb, fdt_image_getnsectors(img));
   fdt_fat_bpb_setbytespersec(bpb, fdt_image_getsectorsize(img));
+  fdt_fat_bpb_setsecpertrk(bpb, fdt_image_getnumberofsector(img));
+  fdt_fat_bpb_setnumheads(bpb, fdt_image_getnumberofhead(img));
 
   return true;
 }
